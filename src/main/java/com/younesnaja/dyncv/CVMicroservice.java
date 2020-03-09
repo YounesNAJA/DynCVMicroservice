c41aeed6b377b567b8958726a4f9f49cdec7a952
Younes NAJA <naja.younes@gmail.com>
2020-03-09 21:02:59 +0100

    Update some names

--- src/main/java/com/younesnaja/dyncv/CVMicroservice.java
+++ src/main/java/com/younesnaja/dyncv/CVMicroservice.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FormationApplication {
+public class CVMicroservice {
 
     public static void main(String[] args) {
-        SpringApplication.run(FormationApplication.class, args);
+        SpringApplication.run(CVMicroservice.class, args);
     }
 
 }
