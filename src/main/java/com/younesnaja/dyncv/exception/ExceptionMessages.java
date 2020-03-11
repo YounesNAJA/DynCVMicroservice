22734a0ffee3c1e541673f14a4733133fc02098a
Younes NAJA <naja.younes@gmail.com>
2020-03-11 16:21:48 +0100

    Remove some unused imports, other minor changes

--- src/main/java/com/younesnaja/dyncv/exception/ExceptionMessages.java
+++ src/main/java/com/younesnaja/dyncv/exception/ExceptionMessages.java
@@ -2,7 +2,7 @@ package com.younesnaja.dyncv.exception;
 
 public abstract class ExceptionMessages {
     public static String PARAMETRE_INVALIDE = "Le format du paramètre %s est invalide";
-    public static String FORMATION_NOT_FOUND = "La formation dont l'id est : %s, est introuvable.";
+    public static String PERSONNE_NOT_FOUND = "La personne que vous cherchez est introuvable.";
 
     public static String getMessage(String message, String... param){
         return String.format(message, param);
