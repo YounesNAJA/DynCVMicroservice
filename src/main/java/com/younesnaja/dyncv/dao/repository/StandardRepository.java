8c6f84537b62b9a2e28df38d8cee7e1e28e7c038
Younes NAJA <naja.younes@gmail.com>
2020-03-15 12:26:14 +0100

    Authorize access to the api from : http://YounesNAJA.com https://YounesNAJA.com

--- src/main/java/com/younesnaja/dyncv/dao/repository/StandardRepository.java
+++ src/main/java/com/younesnaja/dyncv/dao/repository/StandardRepository.java
@@ -1,16 +1,17 @@
 package com.younesnaja.dyncv.dao.repository;
 
-import com.younesnaja.dyncv.dao.domain.StandardDomainObject;
 import com.younesnaja.dyncv.dao.domain.impl.StandardDomainObjectImpl;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin({"http://younesnaja.com", "https://younesnaja.com"})
 @RepositoryRestResource(exported=false)
 public interface StandardRepository<T extends StandardDomainObjectImpl, ID> extends PagingAndSortingRepository<T, BigInteger> {
     @RestResource(path = "id", rel = "id")
